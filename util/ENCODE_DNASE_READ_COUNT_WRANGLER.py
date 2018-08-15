0fee551cc57edc4b6a4cd028f9635ffb95373a41
tacazares <tacazares@gmail.com>
2018-08-15 16:31:51 -0400

    git

--- util/ENCODE_DNASE_READ_COUNT_WRANGLER.py
+++ util/ENCODE_DNASE_READ_COUNT_WRANGLER.py
@@ -1,4 +1,4 @@
-cd /Users/caz3so/Dropbox/thesis/data/20180729_GM12878_DNASE_ENCODE/hg19
+cd /Users/caz3so/Dropbox/thesis/data/20180809_LCL_DNASE_MW_ENCODE/featurecounts//
 
 import pandas as pd
 import numpy as np
@@ -6,40 +6,49 @@ import seaborn as sns
 import matplotlib as plt
 import glob
 
-filenames_hg19 = glob.glob("*hg19.bed_read_length.bed")
-filenames_masked =  glob.glob("*masked.bed_read_length.bed")
+#Here we are creating a ilst of the filenames based on the source
+filenames_hg19 = glob.glob("*_hg19.bed.bam_counts.txt_headless.txt")
+filenames_masked =  glob.glob("*_masked.bed.bam_counts.txt_headless.txt")
+filenames_MW = glob.glob("GM*.bed.bam_counts.txt_headless.txt")
 
-list_of_hg19_dfs = [pd.read_csv(filename, header=None, sep="\t", usecols=[6]) for filename in filenames_hg19]
-list_of_masked_dfs = [pd.read_csv(filename, header=None, sep="\t", usecols=[6]) for filename in filenames_masked]
+#Here we are creating the dataframes based on the list of names
+list_of_hg19_dfs = [pd.read_csv(filename, header=0, usecols=[0,5,6], sep="\t") for filename in filenames_hg19]
+list_of_masked_dfs = [pd.read_csv(filename, header=0, usecols=[0,5,6], sep="\t") for filename in filenames_masked]
+list_of_MW_dfs = [pd.read_csv(filename, header=0, usecols=[0,5,6], sep="\t") for filename in filenames_MW]
 
-df = pd.read_csv('ENCODE_peak_union_ID_lengths.txt', header=0, sep="\t", usecols=[5])
-
+#We then want to create a zipped list of names and DFs and add a name for the source of the data
 zippy1 = zip(list_of_hg19_dfs, filenames_hg19)
 zippy2 = zip(list_of_masked_dfs, filenames_masked)
+zippy3 = zip(list_of_MW_dfs, filenames_MW)
 
+# Here we add the filename as an additional column to every dataframe and remove extra information
 for dataframe, filename in zippy1:
-    dataframe['filename'] = filename.replace("_hg19.bed_read_length.bed", "")
+    dataframe['filename'] = filename.replace("_hg19.bed.bam_counts.txt_headless.txt", "")
 
 for dataframe, filename in zippy2:
-    dataframe['filename'] = filename.replace("_masked.bed_read_length.bed", "")
+    dataframe['filename'] = filename.replace("_masked.bed.bam_counts.txt_headless.txt", "")
 
+for dataframe, filename in zippy3:
+    dataframe['filename'] = filename.replace(".bed.bam_counts.txt_headless.txt", "")
+
+
+# We then concatenate the files into one large frame for each source and add a column with the genome source
 result = pd.concat(list_of_hg19_dfs)
 result["Genome"] = "hg19"
 
 result_masked = pd.concat(list_of_masked_dfs)
 result_masked["Genome"] = "masked"
 
-results_total = pd.concat([result_masked, result])
+result_MW = pd.concat(list_of_MW_dfs)
+result_MW["Genome"] = "MW"
 
-df["Genome"] = "hg19"
-df["filename"] = "Union"
+results_total = pd.concat([result_masked, result, result_MW])
 
-results_all = pd.concat([results_total, df])
-
-y = sns.violinplot(x="filename" , y="Length" , hue="Genome", hue_order=["hg19", "masked"], inner=None, data= results_all, split=True)
+y = sns.violinplot(x="filename" , y="Length", inner=None, data= results_total)
 y.set(yscale="log")
 y.set(ylabel="Length (BP)")
 y.set(xlabel="Sample")
 for item in y.get_xticklabels():
     item.set_rotation(60)
 y.legend(bbox_to_anchor=(1, 1), loc=2)
+
---
+++ util/rodeo_ENCODE_MOTIFS.py
@@ -0,0 +1,66 @@
+cd /Users/caz3so/Dropbox/thesis/data/20180812_LCL_Dnase_hg19/
+
+import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv("GM12878_hg19_UNION_MOTIFS_SUBS.bed", header = None, sep="\t", names=["Chr", "Start", "Stop", "Subcompartment", "Motifs"])
+
+df["Subcompartment"].fillna("Unknown", inplace=True)
+
+df = df.sort_values("Subcompartment")
+
+df
+
+df["Length"] = df["Stop"] - df["Start"]
+
+df['Motifs_count'] = df.Motifs.map(lambda x: [i.strip() for i in x.split(",")])
+
+df['Motifs_count'] = df['Motifs'].str.len()
+
+df["one"] = 1
+
+df["Motifs_peak"] = df["Motifs_count"] / df["one"]
+
+equiv = {"A1":400600000, "A2":581400000, "B1":349400000, "B2":435700000, "B3":855500000, "B4":11000000, "Unknown":248700000}
+
+group_counts = df.groupby(["Subcompartment"])
+group_counts_sum["Sub_length"] = group_counts_sum["Subcompartment"].map(equiv)
+group_counts_sum["Motifs/BP"] = group_counts_sum["Motifs_count"] / group_counts_sum["Sub_length"]
+group_counts_sum["Motifs/Peaks/BP"] = (group_counts_sum["Motifs_count"] / group_counts_sum["one"] ) / group_counts_sum["Sub_length"]
+group_counts_sum["Motifs/Peaks"] = group_counts_sum["Motifs_count"] / group_counts_sum["one"]
+
+#Barplot
+t = sns.barplot(x="Subcompartment", y = "Motifs/Peaks", data=group_counts_sum, palette=pink)
+t.set(xlabel="Sub-compartment")
+t.set(ylabel="TF Motifs/Peaks")
+t.set(title="TF Motifs/Peaks by Sub-compartment")
+
+#boxplot
+b = sns.rugplot(df["Motifs_peak"])
+b.set(xscale="log")
+#Countplot
+y = sns.countplot(x="Subcompartment", data=union_frame, order=["A1", "A2", "B1", "B2", "B3", "B4"], palette=flatui)
+for p in y.patches:
+        y.annotate('{:}'.format(p.get_height()), (p.get_x(), p.get_height()))
+
+y = sns.countplot(x="Subcompartment", data=df, palette=green)
+for p in y.patches:
+        y.annotate('{:}'.format(p.get_height()), (p.get_x(), p.get_height()))
+y.set(ylabel="# of Peaks")
+y.set(title="Peaks per Subcompartment in Union")
+
+#Colors
+flatui = ["#9b59b6", "#3498db", "#95a5a6", "#e74c3c", "#34495e", "#2ecc71"]
+colors = ["windows blue", "amber", "greyish", "faded green", "dusty purple"]
+colorful = sns.xkcd_palette(colors)
+grade = sns.light_palette("green")
+diverging = sns.diverging_palette(10, 220, sep=80, n=7)
+green = sns.cubehelix_palette(8, start=.5, rot=-.75)
+cube = sns.color_palette("cubehelix", 8)
+pink = sns.cubehelix_palette(8)
+teal = sns.color_palette("GnBu_d")
+husl = sns.color_palette("husl", 8)
+
+group_counts_sum.to_csv("groupby_sums.txt")
