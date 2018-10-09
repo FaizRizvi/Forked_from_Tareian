92044c851e9a2226b5341f3d0aa85d764a268d7c
tacazares <tacazares@gmail.com>
2018-10-08 22:00:56 -0400

    partial optimization
    Former-commit-id: f971a29f1ec78b235af303ad75cce64cbe72898c

--- bin/MOTIF_ANALYSIS/TACMAN.py
+++ bin/MOTIF_ANALYSIS/TACMAN.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
-import MOODS.scan
-import MOODS.tools
-import MOODS.parsers
 import snippets
 import os
 import sys
@@ -14,62 +11,59 @@ import pybedtools
 import math
 import numpy as np
 import scipy
-import time
-from tqdm import tqdm
-
+import colored
 from itertools import groupby, chain
+from colored import stylize
+import fish
+
+bird = fish.Bird()
 
 ##########################-----------ARGUMENT PARSER------------##############################
 #Set up the argument parser with all of the options and defaults set up.
 ################################################################################################################
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-v", "--verbosity", action="count", default=0, help='verbose (-vv, -vvv for more)')
-parser.add_argument("-b", "--BED",  help="Input DHS Bed file")
-parser.add_argument("-z", "--MOODS", help="Run Moods", action="store")
-parser.add_argument('-T','--tf', action='store', dest='tf_name_file', help='A tab-delimited file that has TF Name and Motif Name)')
-parser.add_argument("-d", "--domain_bed", dest='domain_bed', help="BED file containing domain calls")
-parser.add_argument("-r", "--RE_bed", dest='RE_bed', help="BED file containing RE annotations")
-parser.add_argument("-c", "--CHIP", nargs='+', action='store', dest='CHIP_bed', help='ChIP BED files', default = [])
-parser.add_argument("-G", "--GEOS_meta", dest='GEOS_meta', help="GEOS meta file from MARIO")
 
-#MOODS OPTIONS - input files
-input_group = parser.add_argument_group("input files (at least one matrix and sequence file required)")
-input_group.add_argument('-m','--matrices', metavar='M', nargs='+', action='store', dest='matrix_files', help='matrix files (count/frequency, will be converted to PWM before matching)', default = [])
-input_group.add_argument('-S','--score-matrices', metavar='M', nargs='+', action='store', dest='lo_matrix_files', help='matrix files (PWM/other score matrix, will be matched directly)', default = [])
-input_group.add_argument('-s','--sequences', metavar='S', nargs='+', action='store', dest='sequence_files', help='sequence files', default = [])
-
-#MOODS OPTIONS - thresholds
-th_group = parser.add_argument_group("threshold selection (exactly one required)")
-th_group.add_argument('-p','--p-value', metavar='p', action='store', dest='p_val', type=float, help='compute threshold from p-value')
-th_group.add_argument('-t','--threshold', metavar='T', action='store', dest='t', type=float, help='use specified absolute threshold')
-th_group.add_argument('-B','--best-hits', metavar='n', action='store', dest='max_hits', type=int, help='return at least the specified amount of best matches')
-
-#MOODS OPTIONS - output
-out_group = parser.add_argument_group("output (optional)")
-out_group.add_argument('-o','--outfile', metavar='outfile', action='store', dest='output_file', help='output to file instead of standard output', required=True)
-out_group.add_argument('--sep', metavar='S', action='store', dest='sep', default=",", help='set field separator in out (default ",")')
-
-#MOODS OPTIONS - behaviour
-option_group = parser.add_argument_group("search and model behaviour (optional)")
-option_group.add_argument('-R','--no-rc', dest='no_rc', action="store_true", help='disable matching versus reverse complement strand')
-option_group.add_argument('--no-snps', dest='no_snps', action="store_true", help='ignore IUPAC symbols coding multiple nucleotides')
-option_group.add_argument('--batch', dest='batch', action="store_true", help='do not recompute thresholds from p-values for each sequence separately (recommended when dealing with lots of short sequences)', default = "batch")
-option_group.add_argument('--bg', metavar=('pA', 'pC', 'pG', 'pT'), nargs=4, action='store', type=float, dest='bg', default = [0.25,0.25,0.25,0.25], help='background distribution for computing thresholds from p-value with --batch (default is 0.25 for all alleles)')
-option_group.add_argument('--ps', metavar='p', action='store', dest='ps', type=float, help='specify pseudocount for log-odds conversion (default = 0.1)', default = 0.01)# bg
-option_group.add_argument('--log-base', metavar='x', action='store', dest='log_base', type=float, help='logarithm base for log-odds conversion (default natural logarithm)')
-option_group.add_argument('--lo-bg', metavar=('pA', 'pC', 'pG', 'pT'), nargs=4, action='store', type=float, dest='lo_bg', default = [0.25,0.25,0.25,0.25], help='background distribution for log-odds conversion (default is 0.25 for all alleles)')
+parser.add_argument("-b", "--BED",  help="Input DHS Bed file", default = [], required=True)
+parser.add_argument('-T','--tf', action='store', dest='tf_name_file', help='A tab-delimited file that has TF Name and Motif Name)', required=True)
+parser.add_argument("-d", "--domain_bed", dest='domain_bed', help="BED file containing domain calls", required=True)
+parser.add_argument("-r", "--RE_bed", dest='RE_bed', help="BED file containing RE annotations", required=True)
+parser.add_argument("-c", "--CHIP", nargs='+', action='store', dest='CHIP_bed', help='ChIP BED files', default = [], required=True)
+parser.add_argument("-G", "--GEOS_meta", dest='GEOS_meta', help="GEOS meta file from MARIO", required=True)
+parser.add_argument("-i", "--MOODS_HITS", nargs='+', action='store', dest='moods_hits', help="Motif hits from MOODS", default = [], required=True)
+parser.add_argument("-MP", "--MP_bool", dest='mp_bool', help="T or F for Moods Parse", required=True)
 
 #set the arguments from the command line to variables in the args object
 args = parser.parse_args()
 
 #set variables from arguments
-MOTIF_HITS = args.output_file
+MOODS_HITS = args.moods_hits
 TF_NAME_FILE = args.tf_name_file
 DOMAIN_BED = args.domain_bed
 RE_BED = args.RE_bed
 CHIP_BED = args.CHIP_bed
 GEOS_META = args.GEOS_meta
+MP_BOOL = args.mp_bool
+
+#Styles
+tacman_color = colored.fg(226) + colored.attr(1)
+
+checkpoint = stylize("################################################################################################################", tacman_color)
+print (checkpoint)
+
+#set up some reference directory file locatios
+cwd = os.getcwd()
+ifd = cwd + "/input"
+ofd = cwd + "/output"
+
+print (stylize("TACMAN: The common working directory is: " + cwd, tacman_color))
+print (stylize("TACMAN: The input directory is: " + ifd, tacman_color))
+
+if not os.path.exists(ofd):
+        os.makedirs(ofd)
+
+print (stylize("TACMAN: The output directory is: " + ofd, tacman_color))
+print(checkpoint)
 
 ####################################------------CHECKPOINT 1-----------------###################################
 # From here on the script will process the motif file that was produced from MOODS or provided as the -o option.
@@ -79,95 +73,94 @@ GEOS_META = args.GEOS_meta
 # Motif_ID		TF_Name
 # M00116_1.97d	TFAP2B
 
-print ("TACMAN: Parsing MOODS")
+MOODS_BED_LIST = []
 
-# Manipulate the list to get the selected feature
-# create a datasframe from the TF_Name list
-TF_df = pd.read_csv(TF_NAME_FILE, sep="\t", header=0)
+if MP_BOOL == "T":
+    print (stylize("TACMAN: Parsing MOODS", tacman_color))
+    print(checkpoint)
+    for i in MOODS_HITS:
+        name = snippets.parse_moods(i, TF_NAME_FILE, ofd)
+        MOODS_BED_LIST.append(name)
+else:
+	print(stylize("TACMAN: Not Parsing MOODS", tacman_color))
 
-# Crete a dictionary of motif names and tf names
-dicted = snippets.dict_TF_df(TF_df)
-
-a = snippets.parse_moods(MOTIF_HITS, dicted)
-
-MOTIF_HITS_BED = MOTIF_HITS.replace(".txt", ".bed")
-fish.animate()
-bird.animate()
-a.sort_values(by=['chr', "TF_start", "TF_end"], inplace=True)
-
-a.to_csv(MOTIF_HITS_BED, sep="\t", index=False, header=False)
-
-####################################------------CHECKPOINT 3-----------------###################################
+####################################------------CHECKPOINT 2-----------------###################################
 # From here the parsed Moods file will be in a BED format and will be intersected with the RE lists of interest and also the domains.
 ################################################################################################################
-print ("TACMAN: Finding Intersections")
-print ("################################################################################################################")
+print(checkpoint)
 
-#TF_domain_intersect = snippets.bed_intersect(MOTIF_HITS_BED, DOMAIN_BED, False)
+MOTIF_PATH = []
 
-#TF_domain_RE_intersect_df =  snippets.bed_intersect(RE_BED, TF_domain_intersect, True)
+for j in MOODS_BED_LIST:
+    print (stylize("TACMAN: Intersecting Domain Bed With: " + j, tacman_color))
+    print(checkpoint)
 
-#TF_domain_RE_intersect_df["RE_ID"] = TF_domain_RE_intersect_df[0] + "_" + TF_domain_RE_intersect_df[1].apply(str) + "_" + TF_domain_RE_intersect_df[2].apply(str)
-#TF_domain_RE_intersect_df["SUB_ID"] = TF_domain_RE_intersect_df[0] + "_" + TF_domain_RE_intersect_df[18].apply(str) + "_" + TF_domain_RE_intersect_df[19].apply(str)
+    TF_domain_intersect = snippets.bed_intersect(j, DOMAIN_BED, True)
 
-#TF_domain_RE_intersect_df.drop([1, 2, 5, 12, 13, 14, 16, 17, 18, 19, 20], axis=1, inplace=True)
+    print(checkpoint)
+    print (stylize("TACMAN: Parsing Domain BED intersected with: " + j, tacman_color))
+    print(checkpoint)
 
-#TF_domain_RE_intersect_df = TF_domain_RE_intersect_df[[0, 6, 7, 8, 9, 10, 11, 15, "RE_ID", "SUB_ID", 3, 4]]
+    MOODS_HITS_BED_PATHS = snippets.domain_parse(TF_domain_intersect, j)
 
-#TF_domain_RE_intersect_df.to_csv("TF_RE_SUB.bed", sep="\t", header = False, index=False)
+    MOTIF_PATH = MOTIF_PATH.append(MOODS_HITS_BED_PATHS)
 
-MOTIF_PATH = os.path.abspath("TF_RE_SUB.bed")
 ####################################------------CHECKPOINT 4-----------------###################################
 # The next step is to import the ChIP files from MARIO and arrange them by run mode, collect them into groups of common TF_start
 # then take the nth percentile and return files either merged or unmerged for each tfself.
 # This will also include parsing the GEOS metadatafile from MARIOS and using it to reference.
 ################################################################################################################
-print ("################################################################################################################")
-print ("TACMAN: Parsing GEOS META")
+print(checkpoint)
+print (stylize("TACMAN: Parsing GEOS META", tacman_color))
+print(checkpoint)
 
-geos_df = snippets.parse_GEOS(GEOS_META)
+CHIP_df = snippets.parse_CHIP(GEOS_META, CHIP_BED)
 
-print ("TACMAN: Parsing ChIP files")
-
-CHIP_group_df = snippets.parse_CHIP(CHIP_BED, geos_df)
-
-print ("TACMAN: Making ChIP folder")
+print(checkpoint)
+print (stylize("TACMAN: Making ChIP folder", tacman_color))
+print(checkpoint)
 
 snippets.make_set_dir("chip")
 
-print ("TACMAN: Parsing the 75th percentile")
+print(checkpoint)
+print (stylize("TACMAN: Parsing the 75th percentile", tacman_color))
+print(checkpoint)
 
-#snippets.percentile_parse(CHIP_group_df, 4)
+snippets.percentile_parse(CHIP_df, 4)
 
-print ("TACMAN: Making BED folder")
+print(checkpoint)
+print (stylize("TACMAN: Making BED folder", tacman_color))
+print(checkpoint)
 
 snippets.make_set_dir("merged")
 
-print ("TACMAN: Merging duplicate BEDS")
+print(checkpoint)
+print (stylize("TACMAN: Merging replicate BEDS", tacman_color))
+print(checkpoint)
 
-#snippets.merge_replicate_BEDS(CHIP_group_df, MOTIF_PATH)
+snippets.merge_replicate_BEDS(CHIP_df, MOTIF_PATH)
 
-print ("TACMAN: Files merged by mode")
+print(checkpoint)
+print (stylize("TACMAN: Files merged by mode", tacman_color))
 
 ####################################------------CHECKPOINT 5-----------------###################################
 # The next step is to take the merged BED files and then create a composite BED file to BIN based on MODE
 ################################################################################################################
-print ("TACMAN: Binning Genome Based on MODES")
-print ("TACMAN: Building reference files")
+print(checkpoint)
+print (stylize("TACMAN: Binning genome based on MODE", tacman_color))
+print(checkpoint)
+
+print (stylize("TACMAN: Building reference files", tacman_color))
 
 MODES = ["MODE1", "MODE2", "MODE3", "MODE4"]
 MODES = pd.DataFrame(MODES)
 
-files_list = []
-
 #In this line of code you will break down the different MODES into sets that will be binned. The next step is sorting.
 MODES[0].apply(snippets.bin_group_collect, args = (CHIP_group_df, MOTIF_PATH, files_list))
-
-snippets.bin_groups_write_files(files_list)
 
 ####################################------------CHECKPOINT 6-----------------###################################
 # The next step is to take the merged BED files and then create a composite BED file to BIN based on MODE
 ################################################################################################################
-print ("TACMAN: Intersecting binned files with ChIP files and MOODS predictions")
+print (stylize("TACMAN: Intersecting binned files with ChIP files and MOODs predictions", tacman_color))
 
 snippets.intersect_bin(MOTIF_PATH)
--- bin/MOTIF_ANALYSIS/snippets.py
+++ bin/MOTIF_ANALYSIS/snippets.py
@@ -13,11 +13,22 @@ import scipy
 import glob
 import time
 from tqdm import tqdm
-
+import colored
 from itertools import groupby, chain
+from colored import stylize
+from collections import OrderedDict
 
 MODES = ["MODE1", "MODE2", "MODE3", "MODE4"]
 MODES = pd.DataFrame(MODES)
+
+#COLOR PARAMETERS
+moods_color = colored.fg(35) + colored.attr(1)
+BED_INTERSECT_color = colored.fg(141) + colored.attr(1)
+DOMAIN_PARSE_color = colored.fg(202) + colored.attr(1)
+MARIO_PARSE_color = colored.fg(13) + colored.attr(1)
+CHIP_PARSE_color = colored.fg(14) + colored.attr(1)
+MAKE_DIR_color = colored.fg(12) + colored.attr(1)
+MERGE_BED_color = colored.fg(196) + colored.attr(1)
 
 def a_bin_parse(x):
     BIN_FILE = []
@@ -29,7 +40,7 @@ def a_bin_parse(x):
     counter = 0
 
     print ("Binning BED: Writing Bins for: " + peaks)
-    for i in tqdm(range(int(bins_zero))):
+    for i in range(int(bins_zero)):
         bin_track = counter * 100
         bin_start = start + bin_track
         bin_end = bin_start + 99
@@ -43,19 +54,19 @@ def a_bin_parse(x):
     return BIN_FILE
 
 def bed_intersect(x, y, z):
-    print ("BED Intersect: Importing BED1")
+    print (stylize("BED Intersect: Importing BED1", BED_INTERSECT_color))
     a = pybedtools.BedTool(x)
 
-    print ("BED Intersect: Importing BED2")
+    print (stylize("BED Intersect: Importing BED2", BED_INTERSECT_color))
     b = pybedtools.BedTool(y)
 
-    print ("BED Intersect: Intersecting BEDS")
+    print (stylize("BED Intersect: Intersecting BEDS", BED_INTERSECT_color))
     a_and_b = a.intersect(b, wa=True, wb=True)
 
-    print ("BED Intersect: Writing BEDS")
+    print (stylize("BED Intersect: Writing BEDS", BED_INTERSECT_color))
     c = a_and_b.moveto("TF_domain.bed")
 
-    print ("BED Intersect: Creating BED files")
+    print (stylize("BED Intersect: Converting to frame", BED_INTERSECT_color))
     df = c.to_dataframe()
 
     if z == True:
@@ -122,17 +133,20 @@ def bin_group_collect(x, df, path, files_list):
 
     BIN_FILE.to_csv(name, sep="\t", header=False, index=False)
 
-def dict_TF_df(x):
-    df = pd.DataFrame()
+def domain_parse(x, y):
+        print (stylize("Domain Parsing: Extracting domain ID", DOMAIN_PARSE_color))
+        x["SUB_ID"] = x[5] + "_" + x[6].apply(str) + "_" + x[7].apply(str)
 
-    df["Motif"] = x["Motif_ID"]
-    df["TF"] = x["TF_Name"]
+        print (stylize("Domain Parsing: Ordering frame", DOMAIN_PARSE_color))
+    	x = x[[0, 1, 2, 4, 3, 8, "SUB_ID"]]
 
-    df.set_index("Motif")
+        print (stylize("Domain Parsing: Replacing extensions", DOMAIN_PARSE_color))
+    	MOODS_HITS_FN = y.replace(".txt", ".bed")
 
-    dicted = dict(zip(df.Motif, df.TF))
+        print (stylize("Domain Parsing: Writing to CSV", DOMAIN_PARSE_color))
+    	x.to_csv(MOODS_HITS_FN, sep="\t", header = False, index=False)
 
-    return dicted
+        return MOODS_HITS_FN
 
 def intersect_bin(x):
     for m in MODES:
@@ -163,14 +177,16 @@ def make_set_dir(x):
     cwd = os.getcwd()
 
     dir = cwd + "/" + x
+    print (stylize("Making Directory: " + dir, MAKE_DIR_color))
 
     if not os.path.exists(dir):
             os.makedirs(dir)
 
+    print (stylize("Making Directory: Changing working directory to: " + dir, MAKE_DIR_color))
     os.chdir(dir)
 
 def merge_replicate_BEDS(x, y):
-    replicate_groups = x["TF_Name"].unique()
+    TF_DF = x["TF_Name"].unique()
 
     rep_group_counts = x.groupby(["TF_Name", "MODE"]).count()
     rep_group_counts.drop([0], axis=1, inplace=True)
@@ -186,15 +202,16 @@ def merge_replicate_BEDS(x, y):
 
     for i in unique_TF_Rep:
         tf_df = x[x.TF_Name == i]
+        print (stylize("Merge BEDS: Extracting domain ID for " + i, MERGE_BED_color))
 
-        for j in MODES:
+        print (stylize("Merge BEDS: Merging " + i, MERGE_BED_color))
+
+        for j in MODES[0]:
             mode_df = tf_df[tf_df.MODE == j]
 
             mode_paths = mode_df["file_path"]
 
-            tf_filename = i + "_" + j + ".bed"
-
-            print ("Merging " + tf_filename)
+            tf_filename = i + "_" + str(j) + ".bed"
 
             with open(tf_filename, 'w') as outfile:
                 for fname in mode_paths:
@@ -213,107 +230,149 @@ def merge_replicate_BEDS(x, y):
 
     for k in unique_TF_single:
         tf_df = x[x.TF_Name == k]
+        print (stylize("Merge BEDS: Parsing TF single file " + k, MERGE_BED_color))
 
-        for l in MODES:
+        for l in MODES[0]:
             mode_df = tf_df[tf_df.MODE == l]
 
             mode_paths = mode_df["file_path"]
 
             tf_filename = k + "_" + l + ".bed"
-            print ("Parsing TF single file " + tf_filename)
             with open(tf_filename, 'w') as outfile:
                 for fname in mode_paths:
                     with open(fname) as infile:
                         outfile.write(infile.read())
 
 def parse_CHIP(x, y):
-    df = pd.DataFrame.from_dict(x)
-
-    df["Basename"] = df[0].apply(os.path.basename)
-    df["File_ext"] = df.Basename.str.split("_").str[-1]
-    df["MODE"] = df.File_ext.str.split(".").str[0]
-    df["Sample_Name"] = df.Basename.str.split("_").str[0]
-
-    df.drop("File_ext", axis=1, inplace=True)
-    df.drop("Basename", axis=1, inplace=True)
-
-    dicted = dict(zip(y.Sample_Name, y.TF))
-
-    df["TF_Name"] = df["Sample_Name"].map(dicted)
-    df.dropna(inplace=True)
-    df["file_path"] = df[0]
-
-    return df
-
-def parse_GEOS(x):
+    print (stylize("MARIO Parsing: Reading in GEOS Meta File", CHIP_PARSE_color))
     df = pd.read_csv(x, sep="\t", header=None, usecols=[1, 13], names=["Sample_Name", "info"])
+    CHIP_df = pd.DataFrame.from_dict(y)
 
+    print (stylize("MARIO Parsing: Splitting strings", CHIP_PARSE_color))
     df["TF"] = df["info"].str.split(":").str[3]
     df["Type"] = df["info"].str.split(":").str[1]
 
-    df.drop(["info"], axis=1, inplace=True)
+    print (stylize("MARIO Parsing: Dropping non-TF ChIP Files", CHIP_PARSE_color))
+
+    histone_mod_list = [  "H3K9me3", "H3K79me2"]
 
     df = df[df.Type != "viral_protein"]
-    histone_markers_list = ["H3K27ac", "H3K27me3", "H3K36me3", "H3K4me1", "H3K4me2", "H3K4me3", "H3K9ac", "H3K9me3", "H3K79me2"]
+    df = df[df.TF != "H3K27ac"]
+    df = df[df.TF != "H3K27me3"]
+    df = df[df.TF != "H3K36me3"]
+    df = df[df.TF != "H3K4me1"]
+    df = df[df.TF != "H3K4me2"]
+    df = df[df.TF != "H3K4me3"]
+    df = df[df.TF != "H3K9ac"]
+    df = df[df.TF != "H3K9me3"]
+    df = df[df.TF != "H3K79me2"]
 
-    for i in histone_markers_list:
-        df = df[df.TF != i]
+    print (stylize("MARIO Parsing: Creating DF ChIP name associations based on GEOS Meta File", CHIP_PARSE_color))
+    CHIP_df["Basename"] = CHIP_df[0].apply(os.path.basename)
+    CHIP_df["File_ext"] = CHIP_df.Basename.str.split("_").str[-1]
+    CHIP_df["MODE"] = CHIP_df.File_ext.str.split(".").str[0]
+    CHIP_df["Sample_Name"] = CHIP_df.Basename.str.split("_").str[0]
 
-    return df
+    print (stylize("MARIO Parsing: Droping unused columns", CHIP_PARSE_color))
+    CHIP_df.drop("File_ext", axis=1, inplace=True)
+    CHIP_df.drop("Basename", axis=1, inplace=True)
 
-def parse_moods(x, y):
-    print ("Parsing MOODS: Reading in CSV")
-    df = pd.read_csv(x, header=None, sep="|")
+    print (stylize("MARIO Parsing: Mapping Names", CHIP_PARSE_color))
+    dicted = dict(zip(df.Sample_Name, df.TF))
 
-    print ("Parsing MOODS: Replacing some extensions")
-    df[1] = df[1].str.replace('_JASPAR.txt.pfm', '')
+    CHIP_df["TF_Name"] = CHIP_df["Sample_Name"].map(dicted)
+    CHIP_df.dropna(inplace=True)
+    CHIP_df["file_path"] = CHIP_df[0]
 
-    print ("Parsing MOODS: Dropping a column")
-    df.drop(6, axis=1, inplace=True)
+    return CHIP_df
 
-    print ("Parsing MOODS: Splitting and creating new frame")
-    df_tmp1 = df[0].str.split(":", expand=True)
+def parse_moods(x, TF_NAME_FILE, ofd):
 
-    print ("Parsing MOODS: Splitting and creating new frame2")
+    def dict_TF_df(x):
+        df = pd.DataFrame()
+
+        df["Motif"] = x["Motif_ID"]
+        df["TF"] = x["TF_Name"]
+
+        df.set_index("Motif")
+
+        dicted = dict(zip(df.Motif, df.TF))
+
+        return dicted
+
+    # create a datasframe from the TF_Name list
+    TF_df = pd.read_csv(TF_NAME_FILE, sep="\t", header=0)
+
+    # Crete a dictionary of motif names and tf names
+    dicted = dict_TF_df(TF_df)
+
+    column_names = ['PEAK_ID', 'PWM_FILE', 'TF_START', 'STRAND', 'MATCH_SCORE', 'MOTIF_SEQ']
+    column_types = {
+        'PEAK_ID': "category",
+        'PWM_FILE': "category",
+        'TF_START': "uint16",
+        'STRAND': "category",
+        'MATCH_SCORE': "float32",
+        'MOTIF_SEQ': "category"
+    }
+
+    print (stylize("Parsing MOODS: Reading in CSV", moods_color))
+    df = pd.read_csv(x, usecols=[0,1,2,3,4,5], names=column_names, dtype=column_types, header=None, sep="|")
+
+    print (stylize("Parsing MOODS: Replacing some extensions", moods_color))
+    df['PWM_FILE'] = df['PWM_FILE'].str.replace('_JASPAR.txt.pfm', '')
+
+    print (stylize("Parsing MOODS: Splitting and creating new frame", moods_color))
+    df_tmp1 = df['PEAK_ID'].str.split(":", expand=True)
+
+    print (stylize("Parsing MOODS: Splitting and creating new frame2", moods_color))
     df_tmp2 = df_tmp1[1].str.split("-", expand=True)
 
-    print ("Parsing MOODS: Dropping another column")
-    df.drop(columns=0, inplace=True)
+    print (stylize("Parsing MOODS: Dropping another column", moods_color))
+    df.drop(columns=['PEAK_ID'], inplace=True)
 
-    print ("Parsing MOODS: Setting Values")
+    print (stylize("Parsing MOODS: Setting Values", moods_color))
     df["chr"] = df_tmp1[0]
     df["start"] = df_tmp2[0]
     df["stop"] = df_tmp2[1]
 
-    print ("Parsing MOODS: Deleting unused frames")
+    print (stylize("Parsing MOODS: Deleting unused frames", moods_color))
     del df_tmp1
     del df_tmp2
 
-    print ("Parsing MOODS: Setting column names")
-    df.columns = ["MOTIF_ID", "TF_POS", "STRAND", "MATCH_SCORE", "MOTIF_SEQ", "chr", "start", "stop"]
+    print (stylize("Parsing MOODS: Calculating TF motif start positions", moods_color))
+    df["TF_start"] = df["start"].apply(int) + 1 + df["TF_START"]
 
-    print ("Parsing MOODS: Calculating TF motif start positions")
-    df["TF_start"] = df["start"].apply(int) + 1 + df["TF_POS"]
-
-    print ("Parsing MOODS: Calculating TF motif end positions")
+    print (stylize("Parsing MOODS: Calculating TF motif end positions", moods_color))
     df["TF_end"] = df["TF_start"] + df["MOTIF_SEQ"].str.len() - 1
 
-    print ("Parsing MOODS: Extracting Peak ID")
-    df["PEAK_ID"] = df["chr"] + "_" + df["start"].map(str) + "_" + df["stop"].map(str)
+    print (stylize("Parsing MOODS: Extracting Peak ID", moods_color))
+    df["PEAK_ID"] = df["chr"] + "_" + df["start"].apply(str) + "_" + df["stop"].apply(str)
 
-    print ("Parsing MOODS: Extracting Motif ID")
-    df["MOTIF_POS"] = df["chr"] + "_" + df["TF_start"].map(str) + "_" + df["TF_end"].map(str)
+    print (stylize("Parsing MOODS: Extracting Motif ID", moods_color))
+    df["MOTIF_POS"] = df["chr"] + "_" + df["TF_start"].apply(str) + "_" + df["TF_end"].apply(str)
 
-    print ("Parsing MOODS: Calculating motif length")
+    print (stylize("Parsing MOODS: Calculating motif length", moods_color))
     df["MOTIF_LEN"] = df["TF_end"] - df["TF_start"] + 1
 
-    print ("Parsing MOODS: Mapping TF names")
-    df["TF_Name"] = df["MOTIF_ID"].map(y)
+    print (stylize("Parsing MOODS: Mapping TF names", moods_color))
+    df["TF_Name"] = df["PWM_FILE"].map(dicted)
 
-    print ("Parsing MOODS: Dropping unused columns")
-    df.drop(columns=["TF_POS", "MOTIF_ID", "MATCH_SCORE", "MOTIF_SEQ", "STRAND", "start", "stop"], inplace=True)
+    print (stylize("Parsing MOODS: Dropping unused columns", moods_color))
+    #df.drop(columns=["TF_START", "PWM_FILE", "MATCH_SCORE", "MOTIF_POS", "MOTIF_LEN", "MOTIF_SEQ", "STRAND", "start", "stop"], inplace=True)
 
-    return df
+    df = df[["chr", "TF_start", "TF_end", "PEAK_ID", "TF_Name"]]
+
+    MOODS_HITS_BED = os.path.basename(x)
+    MOODS_HITS_BED = MOODS_HITS_BED.replace(".txt", ".bed")
+
+    print (stylize("Parsing MOODS: Sorting columns", moods_color))
+    df.sort_values(by=['chr', "TF_start", "TF_end"], inplace=True)
+
+    print (stylize("Parsing MOODS: Writing CSV", moods_color))
+    df.to_csv(MOODS_HITS_BED, sep="\t", index=False, header=False)
+
+    return MOODS_HITS_BED
 
 def percentile_parse(x, y):
     for i in x["file_path"]:
